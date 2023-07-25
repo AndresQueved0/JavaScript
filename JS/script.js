1128cee235ed50aa1f932c28854e1eac989fb78b
Andres <quevedo0504@gmail.com>
2023-07-24 21:47:22 -0500

    ola

---
+++ JS/script.js
@@ -0,0 +1,56 @@
+//console.log("Analisis y Desarrollo de Software")
+
+//alert("Analisis y Desarrollo de Software")
+
+//para comentar en una linea de codigo
+
+//document.write("Analisis y Desarrollo de Software")
+
+//declarar variables var
+
+//var edad =18;
+//const nom="Andres"
+//let num=2.5;
+//Boolean=false;
+
+/* unir dos cadenas de texto
+const nom="Andres"
+const ape="Quevedo"
+alert("mi nombre es " + nom + " y mi apellido es "+ape)*/
+
+/*const num1=10
+const num2=11
+
+const div = num1/num2
+parseInt(div)
+
+alert("la división es: "+div)*/
+
+let nom=prompt("Escriba el nombre")
+
+let edad=prompt("Escriba su edad")
+
+let pes=prompt("Digite su peso")
+
+let ciu=prompt("Escriba su ciudad")
+
+let dirección=prompt("Escriba su dirección")
+
+alert("Mi nombre es: "+nom+ ", mi edad es: "+edad+" años"+ ", mi peso es: "+pes+"kg"+ ", mi ciudad es: "+ciu+ " y mi dirección es: "+dirección)
+
+
+
+/*mostrar en una alerta con los nombres y apellidos completos, edad, peso y dirección*/
+
+
+
+//console.log(typeof Boolean);
+
+//console.log("Mi edad es:"+edad);
+//alert("Mi edad es:"+edad);
+//document.write("Mi edad es:"+edad);
+// simbolo "+" va funcionar para concatenar
+
+//const edad2=22;
+
+//let edad3=33;
